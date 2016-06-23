5f5672d16c54faac4a10c636b05b98e8c4e2ee19
anton-k <anton.kholomiov@gmail.com>
2016-06-23 16:28:50 +0400

    adds root input module

---
+++ src/Graphics/Proc/Lib/Input.hs
@@ -0,0 +1,9 @@
+module Graphics.Proc.Lib.Input(
+	module Graphics.Proc.Lib.Input.Mouse,
+	module Graphics.Proc.Lib.Input.Keyboard,
+	module Graphics.Proc.Lib.Input.Time
+) where
+
+import Graphics.Proc.Lib.Input.Mouse
+import Graphics.Proc.Lib.Input.Keyboard
+import Graphics.Proc.Lib.Input.Time
