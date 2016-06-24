d1e4354d219d32dbe83af77a5395c4af3ed0d73a
anton-k <anton.kholomiov@gmail.com>
2016-06-24 09:29:12 +0400

    fill in top-level module with all re-exports

--- src/Graphics/Proc.hs
+++ src/Graphics/Proc.hs
@@ -1,13 +1,73 @@
 -- | Processing primitves.
 module Graphics.Proc(
+	-- * Structure
 	Proc(..), runProc,
-	Pio,
-
-	Col(..),
+	
+	-- * Types
+	Pio, Draw, Col(..), P2, P3,
 
 	EllipseMode(..),
 
+	-- * Environment
+
+	-- * Data
+	
+	-- * Control
+	
+	-- * Shape
+
+	-- ** 2D Primitives
+
+	triangle, rect, quad, ellipse, circle, line, linePath, point, pointPath, polygon,
+
+	-- ** Curves
+
+	-- bezier, 
+
+	-- * Input
+
+	-- ** Mouse
+	mouse, mouseX, mouseY, 
+	relMouse, relMouseX, relMouseY,
+
+	-- ** Keyboard
+	key, modifiers,
+
+	-- ** Files
+
+	-- ** Time & Date
+	year, month, day, hour, minute, second, millis,
+
+	-- * Output	
+	
+	-- * Transform
+	translate, 
+	rotate, rotateX, rotateY, rotateZ, 
+	scale, 
+	resetMatrix, local, 
+	applyMatrix, 
+	shearX, shearY,
+
+	-- * Lights
+	
+	-- * Camera
+	
+	-- * Color		
+    fill, noFill, stroke, noStroke, strokeFill,
+    rgb, rgba, grey, greya, 
+	background, clear,
+
+	-- * Image	
+
+	-- * Typography
+
+	-- * Math
+
+	-- | Useful standard functions
+	module Data.VectorSpace,
+    module Data.NumInstances,
 	module Data.Default,
+	module Data.Monoid,
 	module Control.Monad,
 	module Control.Applicative
 ) where
@@ -15,7 +75,9 @@ module Graphics.Proc(
 import Data.Default
 import Control.Monad
 import Control.Applicative
+import Data.Monoid
+import Data.VectorSpace hiding (Sum(..))
+import Data.NumInstances
 
-import Graphics.Proc.Core.Pio
-import Graphics.Proc.Core.Run
-import Graphics.Proc.Core.GLBridge
+import Graphics.Proc.Core
+import Graphics.Proc.Lib
