d19967abc129fafb3abc43eb923462b1919df545
anton-k <anton.kholomiov@gmail.com>
2016-06-29 16:49:30 +0400

    trigonometry converters

---
+++ src/Graphics/Proc/Lib/Math/Trigonometry.hs
@@ -0,0 +1,21 @@
+-- | We can use all functions defined in Haskell
+module Graphics.Proc.Lib.Math.Trigonometry(	
+	radians, degrees, e, erad
+) where
+
+import Graphics.Proc.Core
+import Graphics.Proc.Lib.Math.Calculation
+
+radians :: Float -> Float
+radians = remap (0, 360) (0, 2 * pi)
+
+degrees :: Float -> Float
+degrees = remap (0, 2 * pi) (0, 360)
+
+-- | Converts angle in taus to unit vector rotated by given angle.
+e :: Float -> P2
+e x = (cos (2 * pi * x), sin (2 * pi * x))
+
+-- | The function e in radians.
+erad :: Float -> P2
+erad x = (cos x, sin x)
